669eb2dc1f129fd615f30fb9c2068e6383a01626
Marc Abu El Ghait <mabuelgh@cisco.com>
2025-11-10 23:48:59 +0100

    Now compatible with Ubuntu 24.04 and various fixes
    - Temporarily fixed a compatibility issue with current Nvidia drivers and Nvidia container toolkit
    - Fixed issues preventing the scripts to work on Ubuntu 24.04

--- README.md
+++ README.md
@@ -94,7 +94,19 @@ Launch VLLMs with curl containers:
 - Monitor GPUs with commands: "**nvidia-smi**" & "**nvtop**"
 - Steps 1 and 2 are optional if you’re not using Cisco Intersight  
 - Scripts are modular, feel free to adapt them for your environment
-- Tested with ubuntu-22.04.2-live-server on Cisco UCSX-210C-M7 with NVIDIA L40S GPU
+- Tested with **ubuntu-24.04.3-live-server** on Cisco UCSX-210C-M7 with NVIDIA L40S GPU
+
+## Current limitations
+Due to a compatiblity issue right now with supported CUDA in containers, we have to install the drivers manually to **stay in CUDA 12.x.**
+
+CUDA 13 is not yet supported in Nvidia container toolkit. 
+
+We use **nvidia-driver-550** in the current setup file, you may need to adjust it if your GPU is not compatible.
+
+To prevent any upgrade of the NVIDIA drivers, please use:
+```bash
+sudo apt-mark hold nvidia-driver-550 nvidia-kernel-common-550 nvidia-utils-550
+```
 
 ## Features and improvements to come
 - Put scenario 3 python utilisation inside a container instead of on the OS directly
--- setup.sh
+++ setup.sh
@@ -24,8 +24,12 @@ print_subsection_header() {
     echo ""
 }
 
-# Part 1: Append Cisco proxy settings to host environment
-print_section_header "Appending Cisco proxy settings to host environment"
+# Part 1: Append proxy settings to host environment
+print_section_header "Appending proxy settings to host environment"
+export HTTP_PROXY=$PROXY_URL
+export HTTPS_PROXY=$PROXY_URL
+export http_proxy=$PROXY_URL
+export https_proxy=$PROXY_URL
 echo "HTTP_PROXY=$PROXY_URL
 HTTPS_PROXY=$PROXY_URL
 http_proxy=$PROXY_URL
@@ -35,6 +39,9 @@ NO_PROXY=localhost,127.0.0.1,171.*,172.*,192.*,10.*,1.*" | sudo tee -a /etc/envi
 
 # Part 2: Update the package list
 print_section_header "Updating the package list"
+# Remove the interactive dialog during package upgrades, only valable during this script execution
+export DEBIAN_FRONTEND=noninteractive
+sudo needrestart -r a
 sudo apt-get update
 sudo apt upgrade -y
 
@@ -58,16 +65,16 @@ echo \
 sudo apt-get update
 
 print_subsection_header "Installing Docker packages"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-print_subsection_header "Appending Cisco proxy settings to Docker service configuration"
+print_subsection_header "Appending proxy settings to Docker service configuration"
 sudo mkdir -p /etc/systemd/system/docker.service.d
 echo "[Service]
 Environment=\"HTTP_PROXY=$PROXY_URL\"
 Environment=\"HTTPS_PROXY=$PROXY_URL\"
 Environment=\"http_proxy=$PROXY_URL\"
 Environment=\"https_proxy=$PROXY_URL\"
-Environment=\"NO_PROXY=localhost,127.0.0.1,*.cisco.com,171.*,172.*,192.*,10.*,1.*\"" | sudo tee -a /etc/systemd/system/docker.service.d/http-proxy.conf
+Environment=\"NO_PROXY=localhost,127.0.0.1,171.*,172.*,192.*,10.*,1.*\"" | sudo tee -a /etc/systemd/system/docker.service.d/http-proxy.conf
 
 # The Docker service should start automatically after installation
 
@@ -83,8 +90,17 @@ print_section_header "Access the Portainer web interface at http://localhost:900
 
 # Part 4: Install Nvidia Drivers
 print_section_header "Installing Nvidia Drivers"
-sudo ubuntu-drivers autoinstall
-sudo apt-get dist-upgrade
+# sudo ubuntu-drivers autoinstall
+# sudo apt-get dist-upgrade
+# Due to a compatiblity issue right now with supported CUDA in container, we install the drivers manually to stay in CUDA 12.x, 13 is not yet supported in Nvidia container toolkit
+# Remove any existing NVIDIA packages to avoid conflicts
+sudo apt-get purge -y 'nvidia-*'
+sudo apt-get autoremove -y
+# Add NVIDIA's official CUDA repository for Ubuntu 22.04
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt-get update
+sudo apt-get install -y nvidia-driver-550
 
 # Part 5: Install Nvidia container toolkit
 print_section_header "Installing Nvidia container toolkit"
@@ -113,8 +129,22 @@ sudo systemctl restart docker
 
 print_subsection_header "Configuring Nvidia / Docker runtime in rootless mode"
 sudo nvidia-ctk runtime configure --runtime=docker --config=$HOME/.config/docker/daemon.json
-sudo systemctl --user restart docker
-sudo sudo nvidia-ctk config --set nvidia-container-cli.no-cgroups --in-place
+# With the current version of nvidia-ctk, we need to manually add the default-runtime
+print_section_header "Configuring Docker Daemon to use nvidia as default runtime"
+DAEMON_JSON_PATH="/etc/docker/daemon.json"
+NEW_DAEMON_JSON='{
+    "default-runtime": "nvidia",
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}'
+echo "${NEW_DAEMON_JSON}" | sudo tee "${DAEMON_JSON_PATH}" > /dev/null
+sudo systemctl restart docker
+# sudo docker info | grep -i runtime # You should see nvidia as default runtime
+sudo nvidia-ctk config --set nvidia-container-cli.no-cgroups --in-place
 
 # Part 5bis: Install Nvidia CUDA Toolkit
 # print_section_header "Installing Nvidia CUDA Toolkit"
@@ -133,7 +163,11 @@ print_subsection_header "Installing nvtop tool to monitor GPU usage"
 sudo apt-get install nvtop
 
 print_subsection_header "Installing huggingface hub"
-sudo pip install huggingface_hub
+# sudo apt-get install -y python3-venv
+# python3 -m venv ai-bridge
+# source ai-bridge/bin/activate
+# pip install huggingface_hub
+sudo pip install --break-system-packages huggingface_hub
 
 # Part 7: Reboot host machine
 print_section_header "All done! Rebooting now..."
